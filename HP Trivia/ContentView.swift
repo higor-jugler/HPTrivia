fd621fae473ecb8e254773e8d0e477efe733de54
Higor <higorjugler@hotmail.com>
2024-03-11 08:05:06 -0300

    Initial Commit

---
+++ HP Trivia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HP Trivia
+//
+//  Created by Higor Jugler on 3/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
